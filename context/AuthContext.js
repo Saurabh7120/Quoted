4634b8e1cbf86fc97e61cd86847e2c1dd394ddca
Saurabh Mane <saurabhmane1997@gmail.com>
2023-04-04 15:04:31 -0400

    small fix

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -20,7 +20,7 @@ const AuthProvider = ({children}) => {
             if (user) {
               // User is signed in, see docs for a list of available properties
               // https://firebase.google.com/docs/reference/js/firebase.User
-              const {uid, email} = user.uid;
+              const {uid, email} = user;
               const result = await axios.get(`/api/getUser?uid=${uid}`)
               const exists = result.data.exists
               if(exists) {
